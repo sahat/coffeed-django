6c542ff335aec1acc2426455f7c3710dbc392396
Sahat Yalkabov <sakhat@gmail.com>
2013-10-15 11:48:04 -0400

    can now add orders and items from the admin interface

--- coffeed/orders/models.py
+++ coffeed/orders/models.py
@@ -5,14 +5,18 @@ from django.db import models
 
 class Item(models.Model):
     name = models.CharField(max_length=200)
+    count = models.IntegerField(default=0)
+
+    def __unicode__(self):
+        return self.name
 
 
 class Order(models.Model):
     name = models.CharField(max_length=200)
     location = models.CharField(max_length=200)
-    user = models.ForeignKey(User)
     item = models.ManyToManyField(Item)
     created = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True)
 
-
+    def __unicode__(self):
+        return self.location
