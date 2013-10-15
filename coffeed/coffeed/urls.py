28d8706b43bb08afe3a8a87addb11949800456d0
Sahat Yalkabov <sakhat@gmail.com>
2013-10-15 08:11:37 -0400

    switched to simple registration backend (no activation)

--- coffeed/coffeed/urls.py
+++ coffeed/coffeed/urls.py
@@ -6,7 +6,7 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^$', TemplateView.as_view(template_name='base.html')),
-    url(r'^accounts/', include('registration.backends.default.urls')),
+    url(r'^accounts/', include('registration.backends.simple.urls')),
     # Examples:
     # url(r'^$', 'coffeed.views.home', name='home'),
     # url(r'^coffeed/', include('coffeed.foo.urls')),
